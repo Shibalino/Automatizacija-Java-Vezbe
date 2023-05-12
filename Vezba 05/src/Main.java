560420233de87a5ff7127279b04b551057ba5554
Svetlana Kašić <114068205+Shibalino@users.noreply.github.com>
2023-05-12 14:49:40 +0200

    Prvi commit

---
+++ Vezba 05/src/Main.java
@@ -0,0 +1,12 @@
+//Vezba 05
+public class Main {
+    public static void main(String[] args) {
+        /**This is the program that calculates sum of two numbers,
+         * and displays it in the console.*/
+
+        int operand1 = 2;
+        int operand2 = 3;
+        int sum = operand1+operand2;
+        System.out.println(sum);
+    }
+}
