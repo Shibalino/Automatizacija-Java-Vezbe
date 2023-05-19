3f31e08637f48752fa3a6051c4434a104d136134
Svetlana Kašić <114068205+Shibalino@users.noreply.github.com>
2023-05-19 19:55:39 +0200

    Add files via upload

---
+++ Zadatak 02/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+//    Napraviti program koji ce na izlazu prikazati da li je vrednost promenljive koju je
+//    korisnik uneo parna. Takodje treba da bude prikazana uneta vrednost.
+//    Program moze biti resen sa hardkodiranom vrednoscu ili pravim unosom korisnika.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite vrednost promenljive:");
+        int x = scanner.nextInt();
+        if (x%2==0) {
+            System.out.println("Promenljiva je parna.");
+        } else {
+            System.out.println("Promenljiva nije parna.");
+        }
+    }
+}
