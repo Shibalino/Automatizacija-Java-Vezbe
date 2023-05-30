8aff49d13870900680d40e32957b69552b6c53c1
Svetlana Kašić <114068205+Shibalino@users.noreply.github.com>
2023-05-30 20:47:31 +0200

    Add files via upload

---
+++ Vezba 16/src/Main.java
@@ -0,0 +1,15 @@
+//    Vezba 16
+//    Kreirati "for" petlju koja ce prikazati tablicu mnozenja sa 1, 2 i 3 za brojeve od 1 do 10.
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i * 1 + " " + i * 2 + " " + i * 3);
+        }
+    }
+}
+
+
+
